24a41205b32151280eff4ae34d3584fc9c50fe53
BENJAMIN NAIDOO <benjamin.naidoo@apex-rts.co.za>
2026-08-17 22:59:08 +0200

    Added Try and Catch and Log to File Logic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,6 +10,25 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            int[] test = new int[] { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i <= test.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine(test[i]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error occurred: {ex.Message}");
+                    LogToFile(ex);
+                }
+            }
+        }
+
+        private static void LogToFile(Exception ex)
+        {
+            Console.WriteLine($"Logging error to File: {ex}");
         }
     }
 }
